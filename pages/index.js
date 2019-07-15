1d5ae1db07df9ac72e666731594f9a6f5055af55
tobiaswahyudi <tobi8800@gmail.com>
2019-07-15 10:18:59 +0700

    Added check, queue, call and review into index.js

--- pages/index.js
+++ pages/index.js
@@ -2,43 +2,181 @@ import React from 'react'
 import Link from 'next/link'
 import Head from '../components/head'
 import Nav from '../components/nav'
+import axios from 'axios'
 
-const Home = () => (
+const baseUrl = 'http://localhost:8000'//'http://165.22.105.219:8000'
+const checkUrl = baseUrl+'/queue/check/'
+const queueUrl = baseUrl+'/queue/start/'
+const startTalkUrl = baseUrl+'/talk/start/'
+const endTalkUrl = baseUrl+'/talk/end/'
+
+var localPeer
+var localStream = null
+var callConnection = null
+
+function playStream(stream) {
+  var audio = $('<audio autoplay />').appendTo('body');
+  audio[0].srcObject = stream;
+}
+
+async function checkIfQueueIsAllowed(params){
+  return axios.post(checkUrl,params)
+    .then((res)=>
+      {
+        if(res.data.message === 'OK'){
+          return true
+        } else {
+          return false
+        }
+      }
+    )
+    .catch((err)=>{
+      console.log(err)
+      return false
+    })
+}
+
+function getUserID(){
+  var content = $('#userid')[0].value
+  if(content === ''){
+    throw('user id kosong')
+  }
+  return Number(content)
+}
+
+async function tryToQueue(){
+  var isAllowed = await checkIfQueueIsAllowed({/* PAYLOAD HERE */})
+  
+  console.log({isAllowed})
+  
+  if(!isAllowed){
+    alert("Fluent is not open right now. Please come back later!")
+    return
+  }
+
+  var user_id = getUserID()
+  console.log({user_id})
+
+  //instantiate peer
+  localPeer = new Peer({
+    config: {'iceServers': [
+        { url: 'stun:165.22.105.219:65432' },
+        { url: 'turn:165.22.105.219:65432', username: 'peerjs', credential: 'h2olo2' }
+    ]}
+  });
+
+  localPeer.on('open',async()=>{
+    console.log({localPeer})
+    
+    $('.title')[0].innerHTML = localPeer.id
+    
+    navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+    // Get access to microphone
+    navigator.getUserMedia (
+      {video: false, audio: true},
+      
+      // Success callback
+      function success(localAudioStream) {
+          localStream = localAudioStream
+          console.log(localStream)
+      },
+      // Failure callback
+      function error(err) {
+          console.log(err)
+          alert("Audio input device error. Please refresh the website.")
+      }
+    )
+
+    var res = await axios.post(queueUrl,
+      {
+        "topic": "topic",
+        "user_id": user_id,
+        "peerjs_id": localPeer.id
+      },
+      {
+        "headers": {
+          "Content-Type": "application/json"
+        }
+      }
+    )
+    console.log(res.data)
+
+    if(res.data.message === 'Queuing'){
+      alert("Please be patient, we are finding you a partner")
+      localPeer.on('call',async(conn)=>{
+        callConnection = conn
+
+        callConnection.answer(localStream)
+
+        var res = await axios.post(startTalkUrl,
+          {
+            "user_id":user_id
+          },
+          {
+            "headers": {
+              "Content-Type": "application/json"
+            }
+          }
+        )
+        console.log('ini start talk ditembak')
+        console.log(res.data)
+
+
+        var otherID = res.data.user_id
+        var talkID = res.data.talk_id
+
+        callConnection.on('stream', playStream)
+        callConnection.on('close',()=>reviewCallback(otherID,talkID))
+      })
+    } else {
+      
+      alert("Found a partner: "+res.data)
+
+      var otherID = res.data.user_id
+      var talkID = res.data.talk_id
+
+      callConnection = localPeer.call(res.data.peerjs_id,localStream)
+      console.log(callConnection)
+      callConnection.on('stream', playStream);
+      callConnection.on('close',()=>reviewCallback(otherID,talkID))
+    }
+  })
+}
+
+function disconnectCall(){
+  if(callConnection) callConnection.close()
+}
+
+function reviewCallback(otherID,talkID){
+  console.log('###### INI REVIEW CALLBACK #######')
+  console.log('create review for user',otherID,', talkID',talkID)
+}
+
+var Home = () => (
   <div>
-    <Head title="Home" />
+    <Head title="Fluent" />
+    <script src="https://cdn.jsdelivr.net/npm/peerjs@0.3.20/dist/peer.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
     <Nav />
 
     <div className="hero">
-      <h1 className="title">Welcome to Next!</h1>
-      <p className="description">
-        To get started, edit <code>pages/index.js</code> and save to reload.
-      </p>
+      <h1 className="title">hehehe</h1>
+      <table>
+        <tbody>
+          <tr>
+            <td>username:</td>
+            <td><input id="username"></input></td>
+            <td>user ID:</td>
+            <td><input id="userid"></input></td>
+          </tr>
+        </tbody>
+      </table>
 
       <div className="row">
-        <Link href="https://github.com/zeit/next.js#getting-started">
-          <a className="card">
-            <h3>Getting Started &rarr;</h3>
-            <p>Learn more about Next on Github and in their examples</p>
-          </a>
-        </Link>
-        <Link href="https://open.segment.com/create-next-app">
-          <a className="card">
-            <h3>Examples &rarr;</h3>
-            <p>
-              Find other example boilerplates on the{' '}
-              <code>create-next-app</code> site
-            </p>
-          </a>
-        </Link>
-        <Link href="https://github.com/segmentio/create-next-app">
-          <a className="card">
-            <h3>Create Next App &rarr;</h3>
-            <p>Was this tool helpful? Let us know how we can improve it</p>
-          </a>
-        </Link>
+        <button onClick={tryToQueue}>queue me now!</button>
+        <button onClick={disconnectCall}>disconnect call!</button>
       </div>
     </div>
-
     <style jsx>{`
       .hero {
         width: 100%;
