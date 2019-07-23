a9b99f1aaa98b27da9ff8f031650b6e1cef17554
Rikadewi <rika_4444@yahoo.com>
2019-07-23 15:16:34 +0700

    Create responsive mobile page for speak page

--- components/speakPage.js
+++ components/speakPage.js
@@ -7,18 +7,21 @@ class SpeakPage extends Component {
 	render(){
 		return(
 			<div className="columns">
-				<div className="column has-background-link is-narrow">
+				<div className="column has-background-grey-light is-narrow is-hidden-mobile">
 					<div className="columns" style={{width: 300, padding: 30, flexDirection: 'column'}}>         
-						<TileHeader tryToQueue={this.props.tryToQueue} imgsrc='/static/asset/topic/free-purple.svg' title="Free"/>
-						<p className="title" style={{color: '#3C3C72'}}>Talk</p>
-						<p className="subtitle">
-							Talking with stranger and polish your speaking skill. 
-							What do you waiting for? Choose your topic!   
-						</p>
+						<TileHeader tryToQueue={this.props.tryToQueue} imgsrc='/static/asset/topic/free-purple.svg' title="Free Talk"/>
+						<div className="column">
+							<p className="title" style={{color: '#3C3C72'}}>Talk</p>
+							<p className="subtitle">
+								Talking with stranger and polish your speaking skill. 
+								What do you waiting for? Choose your topic!   
+							</p>
+						</div>
 					</div> 
 				</div>
-				<div className="column has-background-primary" style={{padding: 50}}>
+				<div className="column has-background-grey-lighter" style={{padding: 50}}>
 					<div className="columns is-mobile is-multiline"> 
+							<div className="is-hidden-tablet" style={{width: '100%'}}><TileTalk tryToQueue={this.props.tryToQueue} imgsrc='/static/asset/topic/free.svg' title="Free Talk"/></div>
 							<Tile tryToQueue={this.props.tryToQueue} imgsrc='/static/asset/topic/culture.svg' title="Culture"/>
 							<Tile tryToQueue={this.props.tryToQueue} imgsrc='/static/asset/topic/food.svg' title="Food"/>
 							<Tile tryToQueue={this.props.tryToQueue} imgsrc='/static/asset/topic/game.svg' title="Game"/>
@@ -36,6 +39,7 @@ class SpeakPage extends Component {
 	}
 }
 
+
 class Tile extends Component {
 	constructor(props){
 		super(props)
@@ -44,6 +48,19 @@ class Tile extends Component {
 		return (
 			<div className="column container">
 				<Box tryToQueue={this.props.tryToQueue}  title={this.props.title} imgsrc={this.props.imgsrc}/>
+			</div>
+		);
+	}
+}
+
+class TileTalk extends Component {
+	constructor(props){
+		super(props)
+	}
+	render(){
+		return (
+			<div className="column container">
+				<BoxTalk tryToQueue={this.props.tryToQueue}  title={this.props.title} imgsrc={this.props.imgsrc}/>
 			</div>
 		);
 	}
@@ -77,6 +94,33 @@ class Box extends Component {
 						alignItems: 'center',
 						justifyContent:'space-around',
 						height: 300}}>
+					<figure className="image is-128x128">
+						<img src={this.props.imgsrc} />
+					</figure>
+					<p className="title">
+						{this.props.title}
+					</p>
+				</div>
+			</a>
+		);
+	}
+}
+
+class BoxTalk extends Component {
+	constructor(props){
+		super(props)
+	}
+	render() {
+		return (
+			<a onClick={() => this.props.tryToQueue(this.props.title, this.props.imgsrc)}>
+				<div 
+					className="box" 
+					style={{
+						display: 'flex', 
+						flexDirection:'row', 
+						alignItems: 'center',
+						justifyContent:'space-around',
+						height: 200}}>
 					<figure className="image is-128x128">
 						<img src={this.props.imgsrc} />
 					</figure>
