256602f1b6f73c3f259fd816840e3a713f85ec43
tobiaswahyudi <tobi8800@gmail.com>
2019-07-29 10:17:45 +0700

    Reformat review page

--- pages/review.js
+++ pages/review.js
@@ -18,7 +18,7 @@ class Review extends Component {
             var token = sessionManager.getToken()
         }
 
-        this.state = { user: '', clarity: '', pacing: '', pronunciation: '', note: ''}
+        this.state = { user: '', clarity: '', pacing: '', pronunciation: '', note: '', nbspPadding: 31}
         this.handleFeedback = this.handleFeedback.bind(this)
         this.handleClarity = this.handleClarity.bind(this)
         this.handlePacing = this.handlePacing.bind(this)
@@ -100,16 +100,16 @@ class Review extends Component {
               <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
                 <img src="static/asset/icon/user.svg"/>
                   <br></br>
-                  <h1 className="title">Please review your conversation partner!</h1>
+                  <h1 className="title" style={{'text-align': 'center'}}>Please review your conversation partner!</h1>
                   <form onSubmit={this.handleSubmit}>
                     <div className="field is-horizontal">
                       <div className="field-label">
-                        <label className="label" style={{width: '100px', position: 'relative', top:'6px'}}>Clarity</label>
+                        <label className="label" style={{width: '100px', position: 'relative', top:'6px', 'text-align':'left'}}>Clarity</label>
                       </div>
                       <div className="control">
-                        <div className="select" name="clarity">
-                          <select onChange={this.handleClarity}>
-                            <option value="0" disabled selected>Select a rating</option>
+                        <div className="select is-fullwidth" name="clarity">
+                          <select onChange={this.handleClarity} className="is-fullwidth">
+                            <option value="0" disabled selected>Select a rating{'\xa0'.repeat(this.state.nbspPadding)}</option>
                             <option value="1">Difficult to understand</option>
                             <option value="2">Unnatural speech, no intonation</option>
                             <option value="3">Speech is good overall</option>
@@ -121,12 +121,12 @@ class Review extends Component {
                       </div>
                     <div className="field is-horizontal">
                       <div className="field-label">
-                        <label className="label" style={{width: '100px', position: 'relative', top:'6px'}}>Pacing</label>
+                        <label className="label" style={{width: '100px', position: 'relative', top:'6px', 'text-align':'left'}}>Pacing</label>
                       </div>
                       <div className="control">
-                        <div className="select" name="pacing">
-                          <select onChange={this.handlePacing}>
-                            <option value="0" disabled selected>Select a rating</option>
+                        <div className="select is-fullwidth" name="pacing">
+                          <select onChange={this.handlePacing}  style={{'white-space': 'pre'}}>
+                            <option value="0" disabled selected>Select a rating{'\xa0'.repeat(this.state.nbspPadding)}</option>
                             <option value="1">Lots of stops or pauses</option>
                             <option value="2">Too slow</option>
                             <option value="3">Speed is good</option>
@@ -138,12 +138,12 @@ class Review extends Component {
                     </div>
                     <div className="field is-horizontal">
                       <div className="field-label">
-                        <label className="label" style={{width: '100px', position: 'relative', top:'6px'}}>Pronunciation</label>
+                        <label className="label" style={{width: '100px', position: 'relative', top:'6px', 'text-align':'left'}}>Pronunciation</label>
                       </div>
-                      <div className="control">
-                        <div className="select" name="pronunciation">
+                      <div className="control" className="is-fullwidth">
+                        <div className="select is-fullwidth" name="pronunciation">
                           <select onChange={this.handlePronunciation}>
-                            <option value="0" disabled selected>Select a rating</option>
+                            <option value="0" disabled selected>Select a rating{'\xa0'.repeat(this.state.nbspPadding)}</option>
                             <option value="1">Has difficulty pronouncing words</option>
                             <option value="2">Some words are mispronounced</option>
                             <option value="3">Correctly pronounces most words</option>
@@ -154,24 +154,22 @@ class Review extends Component {
                       </div>
                     </div>
                     <div className="field is-horizontal">
-                      <div className="field" style={{width: '100px'}}>
-                        <div className="control" style={{maxWidth: '300px', width: '60vw'}}>
-                          <textarea className="textarea" placeholder="Explain how did your partner perform" onChange={this.handleFeedback}></textarea>
+                      <div className="field" style={{width: '100%'}}>
+                        <div className="control" style={{width: '100%'}}>
+                          <textarea className="textarea" placeholder="Leave some comments on your conversation!" onChange={this.handleFeedback}></textarea>
                         </div>
                       </div>
                     </div>
                     <div className="field is-horizontal">
-                      <div className="field-label">
-                    </div>
                       <div className="field-body">
                         <div className="field is-grouped">
-                          <p className="control">
-                            <button className="button is-primary">
+                          <p className="control" style={{width: '48.5%', 'margin-right': '3%'}}>
+                            <button className="button is-primary" style={{width: '100%'}}>
                               Submit
                             </button>
                           </p>
-                          <p className="control">
-                            <a className="button is-light" onClick={this.handleReport}>Report</a>
+                          <p className="control" style={{width: '48.5%'}}>
+                            <a className="button is-light" onClick={this.handleReport}  style={{width: '100%'}}>Report</a>
                           </p>
                         </div>
                       </div>
